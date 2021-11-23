cab68a8a5350aba8ec2e68c87bc8667322562bf6
Ron Braha <ron.braha@herolo.co.il>
2021-11-23 22:59:14 +0200

    refactor: create app folder and separate providers

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import Routes from 'routes';
-import 'i18n';
-
-function App() {
-  return <Routes />;
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from 'App';
-import StylesProvider from 'styles/Provider';
-import { Provider } from 'react-redux';
-import store from 'store';
+import App from 'app';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <StylesProvider>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </StylesProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
