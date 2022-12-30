f65fd198b8bb9a4e3f107ea9fc5ddf4a07fb128b
Ron Braha <ron.braha@abra-it.com>
2022-12-30 18:14:52 +0200

    build: update to storybook v7

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,25 +1,27 @@
-import type { UserConfig } from 'vite';
-import type { ExtendedOptions } from '@storybook/builder-vite/types';
-import tsconfigPathsPlugin from 'vite-tsconfig-paths';
-import { VitePluginFonts } from 'vite-plugin-fonts';
+import type { StorybookConfig } from '@storybook/builder-vite';
 
-export default {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite',
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
-  viteFinal(config: UserConfig, _options: ExtendedOptions) {
-    // This is mandatory to not override the app's default vite cache dir.
-    config.cacheDir = 'node_modules/.vite-storybook';
-    config.plugins = [
-      ...(config.plugins ?? []),
-      tsconfigPathsPlugin(),
-      VitePluginFonts({
-        google: { families: ['Roboto'] },
+  docs: {
+    autodocs: 'tag',
+  },
+  viteFinal: (config) => {
+    return {
+      ...config,
+      plugins: config.plugins?.filter((value) => {
+        return (value as { name: string }).name !== 'vite-plugin-eslint';
       }),
-    ];
-    return config;
+    };
   },
 };
+
+export default config;
